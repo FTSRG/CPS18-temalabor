70ae7c3a7ce605e31a031ba63424545377940af9
szantob <szantobenedek@gmail.com>
2018-11-27 14:03:13 +0100

    initialize

---
+++ src/java/src/hu/bme/mit/inf/cps/streamprocessor/Main.java
@@ -0,0 +1,13 @@
+package hu.bme.mit.inf.cps.streamprocessor;
+
+import hu.bme.mit.inf.cps._interface.Weather.Temperature;
+
+public class Main {
+	Temperature temp;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
