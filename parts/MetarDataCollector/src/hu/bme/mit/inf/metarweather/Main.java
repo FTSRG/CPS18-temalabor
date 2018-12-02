5db950e74693e7f0017976980f51ed0a147a842a
nkeve <nemeslaki@onlearn.hu>
2018-12-02 12:38:28 +0100

    save

--- parts/MetarDataCollector/src/hu/bme/mit/inf/metarweather/Main.java
+++ parts/MetarDataCollector/src/hu/bme/mit/inf/metarweather/Main.java
@@ -11,13 +11,25 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import com.rti.dds.domain.DomainParticipant;
+import com.rti.dds.domain.DomainParticipantFactory;
+import com.rti.dds.infrastructure.InstanceHandle_t;
+import com.rti.dds.infrastructure.StatusKind;
+import com.rti.dds.publication.Publisher;
+import com.rti.dds.sqlfilter.ParseException;
+import com.rti.dds.topic.Topic;
+
 import hu.bme.mit.inf.weather.weather.AirportWeather.AirportWeatherData;
+import hu.bme.mit.inf.weather.weather.AirportWeather.AirportWeatherDataDataWriter;
+import hu.bme.mit.inf.weather.weather.AirportWeather.AirportWeatherDataTypeSupport;
 import javafx.util.Pair;
 
 public class Main {
@@ -236,5 +248,207 @@ public class Main {
             e.printStackTrace();
         }
     }
+	
+	public static void main(String[] args) {
+		// --- Get domain ID --- //
+        int domainId = 0;
+        if (args.length >= 1) {
+            domainId = Integer.valueOf(args[0]).intValue();
+        }
 
+        // -- Get max loop count; 0 means infinite loop --- //
+        int sampleCount = 0;
+        if (args.length >= 2) {
+            sampleCount = Integer.valueOf(args[1]).intValue();
+        }
+
+        /* Uncomment this to turn on additional logging
+        Logger.get_instance().set_verbosity_by_category(
+            LogCategory.NDDS_CONFIG_LOG_CATEGORY_API,
+            LogVerbosity.NDDS_CONFIG_LOG_VERBOSITY_STATUS_ALL);
+        */
+
+        // --- Run --- //
+        publisherMain(domainId, sampleCount);
+    }
+
+    // -----------------------------------------------------------------------
+    // Private Methods
+    // -----------------------------------------------------------------------
+
+    // --- Constructors: -----------------------------------------------------
+
+    private MetarPublisher() {
+        super();
+    }
+
+    // -----------------------------------------------------------------------
+
+    private static void publisherMain(int domainId, int sampleCount) {
+    	
+    	Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+               @Override
+               public void run() {  
+
+	        DomainParticipant participant = null;
+	        Publisher publisher = null;
+	        Topic topic = null;
+	        AirportWeatherDataDataWriter writer = null;
+	        
+	        
+	
+	        try {
+
+	            // --- Create participant --- //
+	
+	            /* To customize participant QoS, use
+	            the configuration file
+	            USER_QOS_PROFILES.xml */
+	
+	            participant = DomainParticipantFactory.TheParticipantFactory.
+	            create_participant(
+	                domainId, DomainParticipantFactory.PARTICIPANT_QOS_DEFAULT,
+	                null /* listener */, StatusKind.STATUS_MASK_NONE);
+	            if (participant == null) {
+	                System.err.println("create_participant error\n");
+	                return;
+	            }        
+	
+	            // --- Create publisher --- //
+	
+	            /* To customize publisher QoS, use
+	            the configuration file USER_QOS_PROFILES.xml */
+	
+	            publisher = participant.create_publisher(
+	                DomainParticipant.PUBLISHER_QOS_DEFAULT, null /* listener */,
+	                StatusKind.STATUS_MASK_NONE);
+	            if (publisher == null) {
+	                System.err.println("create_publisher error\n");
+	                return;
+	            }                   
+	
+	            // --- Create topic --- //
+	
+	            /* Register type before creating topic */
+	            String typeName = AirportWeatherDataTypeSupport.get_type_name();
+	            AirportWeatherDataTypeSupport.register_type(participant, typeName);
+	
+	            /* To customize topic QoS, use
+	            the configuration file USER_QOS_PROFILES.xml */
+	
+	            topic = participant.create_topic(
+	                "Example Metar",
+	                typeName, DomainParticipant.TOPIC_QOS_DEFAULT,
+	                null /* listener */, StatusKind.STATUS_MASK_NONE);
+	            if (topic == null) {
+	                System.err.println("create_topic error\n");
+	                return;
+	            }           
+	
+	            // --- Create writer --- //
+	
+	            /* To customize data writer QoS, use
+	            the configuration file USER_QOS_PROFILES.xml */
+	
+	            writer = (AirportWeatherDataDataWriter)
+	            publisher.create_datawriter(
+	                topic, Publisher.DATAWRITER_QOS_DEFAULT,
+	                null /* listener */, StatusKind.STATUS_MASK_NONE);
+	            if (writer == null) {
+	                System.err.println("create_datawriter error\n");
+	                return;
+	            }           
+	
+	            // --- Write --- //
+	
+	            /* Create data sample for writing */
+	            AirportWeatherData instance = new AirportWeatherData();
+	
+	            InstanceHandle_t instance_handle = InstanceHandle_t.HANDLE_NIL;
+	            /* For a data type that has a key, if the same instance is going to be
+	            written multiple times, initialize the key here
+	            and register the keyed instance prior to writing */
+	            //instance_handle = writer.register_instance(instance);
+	            
+	            
+	            
+	                	   
+	            
+	                       
+	                  
+	            getMetar();
+	            try {
+					readMetar();
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				}
+	            
+	            final long sendPeriodMillis = 1 * 1000; // 1 seconds
+	
+	            for (int count = 0;
+	            (sampleCount == 0) || (count < sampleCount); ++count) {
+	                System.out.println("Writing Metar, count " + count);
+	
+	                if(count >= metars.size()) {
+	                	metars.clear();
+	                	break;
+	                } 
+	                instance.metadata.location.name = metars.get(count).metadata.location.name;
+	                instance.metadata.timestamp = metars.get(count).metadata.timestamp;
+	                instance.WindDirection = metars.get(count).WindDirection;
+	                instance.WindSpeed = metars.get(count).WindSpeed;
+	                instance.Temperature = metars.get(count).Temperature;
+	                instance.WindGust = metars.get(count).WindGust;
+	                instance.WindDirChange = metars.get(count).WindDirChange;
+	                instance.WindDirChange1 = metars.get(count).WindDirChange1;
+	                instance.DewPoint = metars.get(count).DewPoint;
+	                instance.Pressure = metars.get(count).Pressure;
+	                
+	                /*
+	                MetarInfluxConnector connection = new MetarInfluxConnector();
+	                try {
+						connection.addData(metars.get(count));
+					} catch (ParseException e) {
+						e.printStackTrace();
+					}*/
+					
+	
+	                /* Write data */
+	                writer.write(instance, instance_handle);
+	                try {
+	                    Thread.sleep(sendPeriodMillis);
+	                } catch (InterruptedException ix) {
+	                    System.err.println("INTERRUPTED");
+	                    break;
+	                }
+	                
+	            }
+	                   
+	        
+	            
+	            //writer.unregister_instance(instance, instance_handle);
+	
+	        } finally {
+	
+	            // --- Shutdown --- //
+	
+	            if(participant != null) {
+	                participant.delete_contained_entities();
+	
+	                DomainParticipantFactory.TheParticipantFactory.
+	                delete_participant(participant);
+	            }
+	            /* RTI Data Distribution Service provides finalize_instance()
+	            method for people who want to release memory used by the
+	            participant factory singleton. Uncomment the following block of
+	            code for clean destruction of the participant factory
+	            singleton. */
+	            //DomainParticipantFactory.finalize_instance();
+	        }
+      
+             }
+       }, 0, 1000 * 60 * 60 * 12);       
+    }
+	
 }
